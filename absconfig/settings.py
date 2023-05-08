509a0ae537976614a8fdcb88d706900ba3a56aad
marbleyung <raskolbrd@gmail.com>
2023-05-08 23:58:05 +0300

    add drf-yasg

--- absconfig/settings.py
+++ absconfig/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #pip
+    'rest_framework',
+    'drf_yasg',
+    #apps
 ]
 
 MIDDLEWARE = [
--- absconfig/urls.py
+++ absconfig/urls.py
@@ -17,12 +17,16 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .yasg import urlpatterns as swagger_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls'))
 ]
 
+urlpatterns += swagger_urls
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
